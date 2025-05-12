b42efb35dcf1f0e8a8c1fa43028ebc9b498174c5
felipegith <felipefknowx@gmail.com>
2025-05-12 13:02:11 -0300

    retry pipeline

--- Stefanini.Api/Program.cs
+++ Stefanini.Api/Program.cs
@@ -7,7 +7,6 @@ using Stefanini.Infrastructure.Ioc;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
